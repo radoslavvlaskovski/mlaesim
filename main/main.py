40585056414ca4f2618e36be0844e14f797eb9c1
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2017-02-23 15:15:22 +0100

    imp: add plan

--- main/main.py
+++ main/main.py
@@ -20,4 +20,19 @@ def cluster_test():
     clustering.clustering(dp, 2)
     clustering.clustering(dp, 3)
 
+
+def run_simulation():
+
+    # Think of a comparison function - 70-75% is optimal CPU utilization
+
+    # -> For each time step compute the score of the system
+
+    # Combine the whole algorithm -> classification + regression
+
+    # Train on part of the data
+
+    # Test on the other part + use comparison
+
+    return
+
 cluster_test()
