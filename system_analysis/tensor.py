aedb6e8d26373bf3567a6fb0a09fe4b3c24e1bdb
Radoslav Vlaskovski <radoslav.vlaskovsky@gmail.com>
2017-04-10 20:19:44 +0200

    unknown modifications

--- system_analysis/tensor.py
+++ system_analysis/tensor.py
@@ -1,36 +1,88 @@
-import tensorflow as tf
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import urllib.request
+
 import numpy as np
+import tensorflow as tf
 
-iris_t = "../resources/tensor_tests/iris_training.csv"
-iris_e = "../resources/tensor_tests/iris_test.csv"
+# Data sets
+IRIS_TRAINING = "../resources/tensor_tests/e0_2005_fm.csv"
+IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
 
-def input_fn_train():
-    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-        filename=iris_t, target_dtype=np.int, features_dtype=np.float32)
-    return tf.convert_to_tensor(training_set.data), tf.convert_to_tensor(training_set.target)
-
-
-def input_fn_eval():
-    testing_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-        filename=iris_e, target_dtype=np.int, features_dtype=np.float32)
-    return tf.convert_to_tensor(testing_set.data), tf.convert_to_tensor(testing_set.target)
+IRIS_TEST = "../resources/tensor_tests/e0_2006_fm.csv"
+IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
 def main():
-    # Specify that all features have real-value data
-    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth = True
+  session = tf.Session(config=config)
 
-    # Build 3 layer DNN with 10, 20, 10 units respectively.
-    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                                hidden_units=[2],
-                                                n_classes=3,
-                                                model_dir="/tmp/iris_model")
+  # If the training and test sets aren't stored locally, download them.
+  if not os.path.exists(IRIS_TRAINING):
+    raw = urllib.request.urlopen(IRIS_TRAINING_URL).read()
+    with open(IRIS_TRAINING, "w") as f:
+      f.write(raw)
 
-    # Fit model.
-    classifier.fit(input_fn=input_fn_train,
-                   steps=2000)
+  if not os.path.exists(IRIS_TEST):
+    raw = urllib.request.urlopen(IRIS_TEST_URL).read()
+    with open(IRIS_TEST, "w") as f:
+      f.write(raw)
 
-    # Evaluate accuracy.
-    accuracy_score = classifier.evaluate(input_fn=input_fn_eval)["accuracy"]
-    print('Accuracy: {0:f}'.format(accuracy_score))
+  # Load datasets.
+  training_set = tf.contrib.learn.datasets.base.load_csv_without_header(
+      filename=IRIS_TRAINING,
+      target_dtype=np.int,
+      features_dtype=np.float32,
+      target_column=4)
+  test_set = tf.contrib.learn.datasets.base.load_csv_without_header(
+      filename=IRIS_TEST,
+      target_dtype=np.int,
+      features_dtype=np.float32,
+      target_column=4)
 
-main()
+  # Specify that all features have real-value data
+  feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+
+  # Build 3 layer DNN with 10, 20, 10 units respectively.
+  classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                              hidden_units=[10, 20, 10],
+                                              n_classes=3,
+                                              model_dir="/tmp/iris_model")
+  # Define the training inputs
+  def get_train_inputs():
+    x = tf.constant(training_set.data)
+    y = tf.constant(training_set.target)
+    return x, y
+
+  # Fit model.
+  classifier.fit(input_fn=get_train_inputs, steps=2000)
+
+  # Define the test inputs
+  def get_test_inputs():
+    x = tf.constant(test_set.data)
+    y = tf.constant(test_set.target)
+    return x, y
+
+  # Evaluate accuracy.
+  accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
+                                       steps=1)["accuracy"]
+
+  print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+
+  # Classify two new flower samples.
+  def new_samples():
+    return np.array(
+      [[6.4, 3.2, 4.5, 1.5],
+       [5.8, 3.1, 5.0, 1.7]], dtype=np.float32)
+
+  predictions = list(classifier.predict(input_fn=new_samples))
+
+  print(
+      "New Samples, Class Predictions:    {}\n"
+      .format(predictions))
+
+if __name__ == "__main__":
+    main()
